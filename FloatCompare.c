e81be8ba41304c84cba5e08ffb8fa584a88d1f65
Shyamsundar Parthasarathy <shyam2347@gmail.com>
2014-04-26 05:05:32 -0700

    Create FloatCompare.c

---
+++ FloatCompare.c
@@ -0,0 +1,6 @@
+// Comparison of floating point numbers
+bool IsEqual(double dX, double dY)
+{
+    const double dEpsilon = 0.000001; // or some other small number
+    return fabs(dX - dY) <= dEpsilon * fabs(dX);
+}
