b892c8343554dc7ba39d900871a2e36dc3e8a41a
Shyamsundar Parthasarathy <shyam2347@gmail.com>
2014-04-26 05:17:40 -0700

    Update FloatCompare.c

--- FloatCompare.c
+++ FloatCompare.c
@@ -1,4 +1,5 @@
 // Comparison of floating point numbers
+// Donald Knuth wrote this in Art of Computer Programming - II
 bool IsEqual(double dX, double dY)
 {
     const double dEpsilon = 0.000001; // or some other small number
